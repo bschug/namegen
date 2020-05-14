104500afbbbbb29c85f93f5336a82a487460b25f
Benjamin Schug <benjamin@metagames.gg>
2020-05-14 21:30:33 +0200

    Tweak excuse generator

--- excuses.js
+++ excuses.js
@@ -1,30 +1,30 @@
 INTROS = [
-	"Excuse me,",
 	"Sorry I'm late,",
-	"I'm so sorry, but",
-	"I apologize,",
-	"My deepest apologies,",
-	"Sorry about this,",
-	"So sorry about this,"
+    "Sorry for the delay,",
+	"I know I'm late, but",
+	"I apologize for being late,",
+	"Sorry about the time,",
+	"Sorry about this, but",
+	"So sorry about this, but"
 ];
 
-PEOPLE = ["wife", "husband", "brother", "kid", "kids", "grandma", "grandpa", "mother", "father", "friend", "girlfriend", "boyfriend", "robot butler", "dog", "cat", "hamster", "doctor"];
+PEOPLE = ["wife", "husband", "brother", "kid", "kids", "grandma", "grandpa", "mother", "father", "friend", "girlfriend", "boyfriend", "robot butler", "dog", "cat", "hamster", "doctor", "chicken"];
 
-PLACES_FOR_PEOPLE = ["doctor", "airport", "supermarket", "pharmacy", "hospital", "cemetary", "zoo", "school", "university", "furniture store", "asylum", "lawyer"];
+PLACES_FOR_PEOPLE = ["doctor", "airport", "supermarket", "pharmacy", "hospital", "cemetary", "zoo", "school", "university", "furniture store", "asylum", "divorce lawyer"];
 
-THINGS_THAT_BREAK = ["car", "computer", "laptop", "monitor", "mouse", "keyboard", "oven", "fridge", "vr headset", "body", "brain", "shoes", "Unity", "Photoshop", "Excel", "bicycle", "motorcycle", "bicycle helmet", "motorcycle helmet", "bed", "chair", "table", "phone", "excuse generator"];
+THINGS_THAT_BREAK = ["car", "computer", "laptop", "monitor", "mouse", "keyboard", "oven", "fridge", "VR headset", "body", "brain", "shoes", "Unity", "Photoshop", "Excel", "bicycle", "motorcycle", "bicycle helmet", "motorcycle helmet", "bed", "chair", "table", "phone", "excuse generator"];
 
-ACTIVITIES = ["driving", "sleeping", "on my way home", "doing groceries", "finishing my tasks", "working overtime", "being busy", "working hard", "going to the airport", "on the train", "swimming", "taking a shower", "feeding my cat", "feeding my dog", "on the phone", "binge drinking", "smoking weed", "faking statistics", "watering the plants", "playing video games"];
+ACTIVITIES = ["driving", "sleeping", "on my way home", "doing groceries", "finishing my tasks", "working overtime", "being busy", "working hard", "going to the airport", "on the train", "swimming", "taking a shower", "feeding my cat", "feeding my dog", "on the phone", "binge drinking", "smoking weed", "faking statistics", "watering the plants", "playing video games", "planning", "drinking coffee", "doing drugs", "coming up with an excuse"];
 
-THINGS_TO_LOSE = ["glasses", "wallet", "sanity", "beer", "shit", "password", "money", "Facebook password", "Trello account", "keys", "inheritance", "claim to the throne", "trust"];
+THINGS_TO_LOSE = ["glasses", "wallet", "sanity", "beer", "shit", "password", "money", "Facebook password", "Trello account", "keys", "inheritance", "claim to the throne", "trust", "shirt", "clothes", "underwear", "cocaine", "argument", "dignity", "excuse", "homework"];
 
 THINGS_TO_CRASH = ["computer", "car", "bicycle", "motorcycle", "Unity", "Photoshop", "Playstation", "stock market portfolio"];
 
-EMOTIONS = ["angry", "sad", "depressed", "happy", "hungry", "sick", "impatient", "annoying", "annoyed", "excited"];
+EMOTIONS = ["angry", "sad", "depressed", "happy", "hungry", "sick", "impatient", "annoyed", "excited"];
 
 DESTROYED = ["lost", "crushed", "dropped", "burned", "broke", "forgot", "ate"];
 
-TIMES = ["yesterday", "today", "now", "the other day", "last year", "just now", "recently"];
+TIMES = ["yesterday", "today", "now", "the other day", "last year", "just now", "recently", "this morning"];
 
 function generateExcuse()
 {
@@ -45,7 +45,12 @@ function generateReason()
 		myXWasStolen,
 		myXAteMyY,
 		someonesEmotion,
+		iWasBusy
 	])();
+}
+
+function iWasBusy() {
+	return "I was busy " + activity();
 }
 
 function iHadToTakeXtoY() { 
@@ -57,7 +62,7 @@ function myXBrokeDownWhileY() {
 }
 
 function iLostSomething() {
-	return "I lost " + someones() + draw(THINGS_TO_LOSE) + when();
+	return someoneActive() + " lost " + someones() + draw(THINGS_TO_LOSE) + when();
 }
 
 function destroyedSomething() {
@@ -65,15 +70,15 @@ function destroyedSomething() {
 }
 
 function myXWasStolen() {
-	return someoneActive() + " stole " + someones() + draw(THINGS_TO_LOSE.concat(THINGS_THAT_BREAK)) + when();
+	return someoneElse() + " stole " + someones() + draw(THINGS_TO_LOSE.concat(THINGS_THAT_BREAK).concat(PEOPLE)) + when();
 }
 
 function myXAteMyY() {
-	return someoneActive() + " ate " + someones() + draw(THINGS_TO_LOSE.concat(THINGS_THAT_BREAK)) + when();
+	return someoneActive() + " ate " + someones() + draw(THINGS_TO_LOSE.concat(THINGS_THAT_BREAK).concat(PEOPLE)) + when();
 }
 
 function someonesEmotion() {
-	return someoneActive() + " got " + draw(EMOTIONS) + " because " + someoneWas() + draw(ACTIVITIES) + when();
+	return someoneActive() + " got " + draw(EMOTIONS) + " because " + someoneWas() + activity() + when();
 }
 
 function someones() {
@@ -81,7 +86,7 @@ function someones() {
 }
 
 function someoneActive() {
-	return draw(["I", "you", "someone", "my " + draw(PEOPLE)]);
+	return draw(["I", "my " + draw(PEOPLE)]);
 }
 
 function someonePassive() {
@@ -89,19 +94,30 @@ function someonePassive() {
 }
 
 function someoneWas() {
-	return draw(["I was ", "you were ", "someone was ", someones() + draw(PEOPLE) + " was "]);
+	return draw(["I was ", someones() + draw(PEOPLE) + " was "]);
+}
+
+function someoneElse() {
+	return "my " + draw(PEOPLE);
+}
+
+function activity() {
+	return draw([
+		function() { return draw(ACTIVITIES) },
+		function() { return draw(ACTIVITIES) + " with " + someoneElse() }
+	])();
 }
 
 
 function when() {
 	return draw([
-		"",
-		" " + draw(TIMES),
-		" while I was " + draw(ACTIVITIES),
-		" at the " + draw(PLACES_FOR_PEOPLE),
-		" while I was taking my " + draw(PEOPLE) + " to the " + draw(PLACES_FOR_PEOPLE),
-		", so I spent too much time " + draw(ACTIVITIES),
-		" because I was busy " + draw(ACTIVITIES),
-		" because " + someoneWas() + draw(ACTIVITIES)
-	]);
+		function() { return "" },
+		function() { return " " + draw(TIMES)},
+		function() { return " while I was " + draw(ACTIVITIES)},
+		function() { return " at the " + draw(PLACES_FOR_PEOPLE)},
+		function() { return " while I was taking my " + draw(PEOPLE) + " to the " + draw(PLACES_FOR_PEOPLE)},
+		function() { return ", so I spent too much time " + draw(ACTIVITIES)},
+		function() { return " because " + generateReason() },
+		function() { return " when " + generateReason() }
+	])();
 }
