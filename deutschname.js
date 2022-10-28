68b38e3c249b4088e1c1f1191feaad090268821e
Benjamin Schug <benjamin@metagames.gg>
2022-10-28 19:02:09 +0200

    Add more names and support shift-click in Deutschname

--- deutschname.js
+++ deutschname.js
@@ -1,9 +1,9 @@
 var firstnamePrefixes = [
 	"adel", "anne", "bar", "bertels", "chris", "elisa",
 	"glücks", "gott", "gram", "groß",
-	"inge", "irm", "klein", "mett", "nor", "nar", "mar", "pu"];
+	"inge", "irm", "klein", "man", "mett", "nor", "nar", "mar", "pu"];
 var firstnameSuffixes = [
-	"bara", "beth", "bold", "era", "fred", "frieda",
+	"bara", "beth", "bette", "bold", "dred", "era", "fred", "frieda",
 	"gang", "gard", "garst", "gret", "grid",
 	"-Heinz", "hold", "horst", "heid", "fina", "finchen", "fine", "lotte",
 	"mann", "marie", "minza", "mette", "rich", "rieke",
@@ -11,7 +11,7 @@ var firstnameSuffixes = [
 var firstnameAny = [
 	"bert", "borg", "fried", "friede", "hans", "hänsel", "hilde",
 	"josef", "karl", "max", "muckel", "peter", "toffel", "tor",
-	"wolf", "wurst"];
+	"wenzel", "wolf", "wurst"];
 
 var firstnamePrefixCounts = [
 	new WeightedValue(1, 100),
@@ -24,7 +24,8 @@ var conjections = [
 ];
 
 var lastnamePrefixes = [
-	"alt", "bären", "braun", "brumm", "butzel", "doppel", "dunkel", "erd", "frauen", "fein", "funken",
+	"alt", "bären", "braun", "brumm", "butzel", "doppel", "dunkel", "erd", 
+	"frauen", "fein", "funken",
 	"glücks", "groß", "hart", "hasen", "heiß", "hinter", "hoch", "hunds",
 	"klein", "königs", "kröten", "krumm", "kuschel", "manns", "pfoten", "pranken", "prinzen", "roll", "rund",
 	"sauf", "schlangen", "schlängel", "schnell", "schweins",
@@ -33,17 +34,20 @@ var lastnamePrefixes = [
 	"über", "un", "unter", "weibs", "weich", "woll", "wolfs"];
 var lastnameSuffixes = [
 	"bär", "bauch", "blatt", "brand", "brust", "brunst",
-	"feind", "fleischer", "flink", "fratze", "frau", "freude", "freund", "funke", "glück",
-	"haft", "hase", "hund", "ikowski", "kind", "könig", "kröte", "lich", "los", "mann", "meier", "milch",
+	"feind", "fleischer", "flink", "fratze", "frau", "freude", "freund", "funke", "gartner", "glück", "haft", "hase", "heber", "hund", 
+	"ikowski", "kind", "könig", "kröte", "lich", "los", "mann", "meier", "milch",
 	"pfote", "pranke", "prinz", "quell", "richter", "riese",
 	"säufer", "schenk", "schlange", "schrat", "schuh", "schwein",
-"steiger", "sucht", "sonne", "spinner",
+    "steiger", "sucht", "sonne", "spinner",
 	"teufel", "träger", "weib", "wolf", "zwerg" ];
 var lastnameAny = [
-	"arm", "bart", "bass", "baum", "berg", "bett", "bier", "brut", "blut", "dackel", "dödel",
+	"arm", "bart", "bass", "baum", "berg", "bett", "bier", "brut", "blut", 
+	"dackel", "dödel",
 	"fass", "feuer", "fleisch", "fuchs", "führer", "fuß",
-	"gier", "gold", "hammer", "hand", "haus", "herz", "hoden", "holz", "huhn", "jäger",
-	"klöten", "kopf", "kraft", "leder", "list", "lust", "maul", "maus", "mund", "nest", "nacken",
+	"gier", "gold", 
+	"hammer", "hand", "haupt", "haus", "herz", "hoden", "holz", "huhn", "jäger",
+	"klöten", "kopf", "kraft", "leder", "list", "lust", 
+	"maul", "maus", "mund", "nest", "nacken",
 	"rauch", "reich", "rödel",
 	"sau", "schaf", "schelm", "schlingel", "schlumpf", "schnee", "schreck", "stein",
 	"stolz", "silber", "stark", "stern", "strick", "sturm", "trick",
